5c68664e16ecc74b7b4e614af5329c010edae359
Jules Drean <drean@mit.edu>
2023-04-28 18:59:46 +0000

    Fix bug

--- os_entry.c
+++ os_entry.c
@@ -24,21 +24,13 @@ void untrusted_main(int core_id, uintptr_t fdt_addr) {
     riscv_perf_cntr_begin();
 
     for(int i = 0; i < NUM_SIGN; i++) {
-      local_hash(a[i%len_a], len_elements[i%len_a], &pk, &sk, &sigs[i]);
+      local_hash(a[i%len_a], len_elements[i%len_a], &sigs[i]);
     }
 
     riscv_perf_cntr_end();
     // *** END BENCHMARK ***
-    
-    printm("End benchmark starts verification\n");
 
     bool res = true;
-    for(int i = 0; i < NUM_SIGN; i++) {
-      //printm("sigs[%x] %d\n", i, sigs[i].bytes[0]);
-      res &= local_verify(&sigs[i], a[i%len_a], len_elements[i%len_a], &pk);
-    }
-    printm("Verification %s\n", (res ? "is successful": "has failed"));
-
     printm("End experiment\n");
     int cmd = (res == true) ? 0: 1;
     send_exit_cmd(cmd);
